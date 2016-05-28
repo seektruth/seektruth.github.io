3fac81a6d3165250f5e0487e6f3a821bd4421991
seektruth <seektruthalone@163.com>
2016-05-28 19:47:13 +0800

    特定酒店模板完成

---
+++ search.php
@@ -0,0 +1,20 @@
+<?php  
+    echo 
+    '{
+        "杭州紫金港特大酒店":
+        {
+        "city": "成都",
+        "province": "浙江",
+        "location": "西湖区 申花路798号",
+        "rank": 3.5,
+        "amount": 1,
+        "room": 
+            {
+                "经济间": 129,
+                "标准间": 345
+            },
+        "lowest price": 129,
+        "picture": "hotel/0001.jpg"
+        }
+    }'
+?>
